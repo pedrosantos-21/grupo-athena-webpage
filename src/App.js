a2bb9cfea9524e6793edd5d9fbc46c27778b565c
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2023-08-28 13:18:58 -0300

    Initial Commit 🎉

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import './styles.css';
+
+function App() {
+  return (
+      <div className="center">
+        <nav className="menu-container">
+          <a href="#inicio-pagina">Home</a>
+          <a href="#sobre-o-athena">Sobre</a>
+          <a href="#trabalhe-conosco">Faça Parte</a>
+          <a href="#contatos">Contato</a>
+        </nav>
+      
+      <div className="center">
+        <img className="logotipo-athena" src="https://th.bing.com/th/id/OIP.3M-6pP3ls49hOIYJ3s_FzQHaEo?pid=ImgDet&rs=1" alt="Logo Athena" />
+        <h1>Grupo Athena</h1>
+        <p>Desvendando o Conhecimento e Explorando Novas Fronteiras</p>
+        <h2>#CodeLikeAGirl ❤</h2>
+        <h2>#GirlsWhoCode 👩‍💻</h2>
+      </div>
+
+      <main>
+      <section id="inicio-pagina" class="titulos-site">
+  <p class="legenda-titulo-site">Somos um grupo formado por meninas dos cursos de Exatas do Centro Universitário de Maceió - Unima/Afya com o propósito em incentivar as estudantes durante a graduação na área de STEAM.</p>
+  <h2 id="sobre-o-athena" class="sobre-o-athena">Sobre o Grupo Athena</h2>
+  <p>O Grupo Athena é uma iniciativa inspiradora e vibrante composta por estudantes do sexo feminino que estão matriculadas em cursos relacionados às Ciências Exatas na Unima/Afya, em parceria com a Afya Educacional. Com uma visão audaciosa e uma missão poderosa, o grupo se dedica a incentivar e apoiar ativamente suas colegas durante sua jornada educacional nas áreas de STEAM (Ciência, Tecnologia, Engenharia, Artes e Matemática).</p>
+  <p>Cada membro do Grupo Athena é movido por uma paixão compartilhada pela ciência e pelas disciplinas exatas, reconhecendo a importância dessas áreas para a sociedade contemporânea e o progresso global. Através de reuniões regulares, workshops interativos, palestras inspiradoras e eventos envolventes, o grupo trabalha para promover uma comunidade inclusiva e de apoio, onde as estudantes possam se sentir fortalecidas e encorajadas a perseguir suas ambições acadêmicas e profissionais.</p>
+  <p>Uma das características distintivas do Grupo Athena é seu compromisso com o empoderamento feminino em um campo tradicionalmente dominado por homens. Ao reunir mulheres que compartilham interesses comuns, o grupo cria um espaço seguro e estimulante onde as estudantes podem trocar experiências, compartilhar desafios e superar obstáculos juntas. Essa rede de apoio é essencial para garantir que nenhuma estudante se sinta isolada ou desencorajada em sua busca por conhecimento e sucesso</p>
+  <p>Os projetos e atividades desenvolvidos pelo Grupo Athena são variados e inovadores. Desde hackathons e competições até projetos de voluntariado e ações comunitárias, o grupo busca constantemente formas de se envolver com a comunidade local, ampliar sua influência positiva e promover a igualdade de gênero no campo das Ciências Exatas. Além disso, parcerias com profissionais e empresas do setor permitem que as estudantes tenham acesso a orientações práticas, conselhos de carreira e oportunidades de estágio.</p>
+  <p>Athena, a deusa grega da sabedoria, coragem e inspiração, serve como símbolo e guia para o grupo, representando os ideais de conhecimento, força e liderança que cada membro busca alcançar. O Athena não apenas apoia suas estudantes individualmente, mas também trabalha para impactar positivamente a sociedade em geral, incentivando mais mulheres a entrar nas áreas de STEAM e contribuir para avanços científicos, tecnológicos e sociais.</p>
+  <p>Em resumo, o Grupo Athena é muito mais do que apenas um coletivo de estudantes. É uma comunidade apaixonada e dinâmica que acredita no potencial ilimitado das mulheres nas Ciências Exatas. Com seu compromisso com a diversidade, inclusão e excelência, o Athena está moldando um futuro mais igualitário e empoderado para as mulheres que buscam trilhar um caminho notável no mundo das STEAM.</p>
+  <h3 id="trabalhe-conosco" class="faca-parte-do-athena">Venha fazer parte do Grupo Athena!!!</h3>
+  <div class="center">
+    <img class="foto-athena" src="https://th.bing.com/th/id/R.fe87a4954808b79d912400d8f792e0a0?rik=Taac7DCj45KAkQ&pid=ImgRaw&r=0" alt="Foto Athena" /> 
+  </div>
+  <p>Prezadas estudantes,</p>
+  <p>Você já pensou em fazer parte de um grupo dinâmico e inspirador que não apenas apoia suas aspirações acadêmicas, mas também promove a igualdade de gênero nas áreas de Ciência, Tecnologia, Engenharia, Artes e Matemática (STEAM)? O Grupo Athena é exatamente essa oportunidade que você estava esperando para se destacar e fazer a diferença!</p>
+  <p>Imagine um ambiente acolhedor onde você pode compartilhar seus interesses pelas disciplinas exatas com outras mulheres incríveis, todas elas com um objetivo em comum: superar barreiras e alcançar patamares elevados em suas carreiras. Ao se juntar ao Athena, você terá acesso a uma rede de apoio dedicada a incentivá-la a enfrentar desafios, superar obstáculos e aproveitar ao máximo sua jornada educacional.</p>
+  <p>Aqui estão algumas razões convincentes para você considerar ingressar no Grupo Athena:</p>
+  <ul>
+    <li>Empoderamento Coletivo: Fazemos parte de um movimento que busca empoderar mulheres nas áreas de STEAM. Ao se unir a nós, você se tornará parte de uma comunidade que está moldando o futuro e abrindo caminhos para futuras gerações.</li>
+    <li>Rede de Apoio: Ao se tornar membro, você não estará sozinha em sua jornada. Terá acesso a uma rede forte de colegas, mentoras e profissionais que podem oferecer orientação, apoio emocional e conselhos práticos.</li>
+    <li>Eventos Inspiradores: O Grupo Athena oferece uma variedade de workshops, palestras e eventos envolventes, proporcionando oportunidades únicas de aprendizado e crescimento pessoal.</li>
+    <li>Desenvolvimento de Habilidades: Participar de nossas atividades ajudará você a desenvolver habilidades de liderança, comunicação e colaboração, que são essenciais para o sucesso em qualquer carreira.</li>
+    <li>Visibilidade Profissional: Colaboramos com empresas e profissionais do setor, proporcionando chances de networking e até mesmo estágios valiosos para impulsionar sua carreira.</li>
+    <li>Impacto Social: Ao se envolver conosco, você não apenas beneficia sua própria trajetória, mas também contribui para um mundo mais equitativo, onde mulheres têm um papel de destaque nas áreas de STEAM.</li>
+    <li>Amizades Duradouras: Além do desenvolvimento profissional, você construirá amizades duradouras com pessoas que compartilham seus interesses e paixões.</li>
+  </ul>
+  <p>Agora é a hora de dar um passo audacioso em direção a um futuro brilhante. O Athena está à espera de mentes curiosas, determinadas e apaixonadas para se unirem a nós nessa jornada emocionante. Juntas, podemos quebrar barreiras, alcançar nossos objetivos e inspirar outros a seguirem nossos passos.</p>
+  <p>Não perca a oportunidade de ser parte dessa transformação. Junte-se ao Grupo Athena e seja uma força motriz na promoção da igualdade de gênero e no avanço das Ciências Exatas. Estamos ansiosas para recebê-la de braços abertos!</p>
+  <div class="center">
+    <button class="inscreva-se-button"><a href="https://docs.google.com/forms/d/e/1FAIpQLSd_p8HHTq9e-r5gLgjRZmnsKrIbMwpUEmPzfM5Uf82LfpB1xQ/viewform?usp=sf_link" target="_blank">Inscreva-se</a></button>
+    <section>
+  <div class="center">
+    <img class="foto-representante-athena" src="https://avatars.githubusercontent.com/u/94319702?v=4" alt="Representante Athena Unima/Afya" />
+    <div class="info">
+      <h2 class="nome-representante">Maria de Fátima Nunes Alves</h2>
+      <p class="cargo-representante-grupo-athena">Presidente e Desenvolvedora Full-Stack Jr. do Grupo Athena</p>
+    </div>
+  </div>
+</section>
+  </div>
+</section>
+
+        <section>
+        <h4 id="contatos" class="contato">Contato</h4>
+        <p>🌟 Descubra o Mundo STEAM com o Grupo Athena! 🌟</p>
+        <p>Meninas incríveis, vocês estão prontas para uma jornada emocionante rumo ao empoderamento nas áreas de Ciência, Tecnologia, Engenharia, Artes e Matemática (STEAM)? O Grupo Athena está aqui para vocês, e agora estamos mais conectadas do que nunca!</p>
+        <p>Siga-nos nas redes sociais para entrar em um universo de oportunidades inspiradoras e compartilhar suas paixões. Juntas, podemos criar uma comunidade de mulheres determinadas que estão moldando o futuro e desafiando limites.</p>
+        <div class="redes-sociais">
+        <a href="https://www.facebook.com/grupoathenaunit" target="_blank"><img class="logotipo-facebook" src="https://img.icons8.com/?size=512&id=118497&format=png" /></a>
+        <a href="https://instagram.com/grupoathenaunima" target="_blank"><img class="logotipo-instagram" src="https://img.icons8.com/?size=512&id=Xy10Jcu1L2Su&format=png" /></a>
+        <a href="https://br.linkedin.com/company/grupo-athena-unit" target="_blank"><img class="logotipo-linkedin" src="https://img.icons8.com/?size=512&id=xuvGCOXi8Wyg&format=png" /></a>
+        </div>
+
+        <p className="email-grupo-athena">E-mail: grupoathenaunimaafya@gmail.com</p>
+        </section>
+      </main>
+
+      <footer>
+        <p>2023 - Grupo Athena Unima/Afya&reg;</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
